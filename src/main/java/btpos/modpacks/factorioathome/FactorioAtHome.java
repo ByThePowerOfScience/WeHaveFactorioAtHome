7685b356efda5177d7c2d46b43c9bf89823a8341
ByThePowerOfScience <public.bythepowerofscience@gmail.com>
2023-09-21 15:09:05 -0400

    Initial commit

---
+++ src/main/java/btpos/modpacks/factorioathome/FactorioAtHome.java
@@ -0,0 +1,13 @@
+package btpos.modpacks.factorioathome;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid=FactorioAtHome.MODID, name=FactorioAtHome.NAME, version = FactorioAtHome.VERSION, dependencies = FactorioAtHome.DEPENDENCIES)
+public class FactorioAtHome {
+	public static final String MODID = "factorioathome";
+	public static final String NAME = "We Have Factorio at Home";
+	public static final String VERSION = "@VERSION@";
+	public static final String DEPENDENCIES = "";
+	
+	
+}
---
+++ src/main/java/btpos/modpacks/factorioathome/multiblock/BlockMultiblock.java
@@ -0,0 +1,15 @@
+package btpos.modpacks.factorioathome.multiblock;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+
+import javax.annotation.Nullable;
+
+public class BlockMultiblock extends Block {
+	
+	public BlockMultiblock(Material materialIn) {
+		super(materialIn);
+	}
+}
